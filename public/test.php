afec2cae118fe4c29af0ec9d2eff2a80dfc0f22d
Denis Zhurov <den_fobos@mail.ru>
2021-09-11 22:24:58 +0300

    clean directory

--- public/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require_once '../app/bootstrap.php';
-    echo ROOT_DIR . '<br>';
-    echo 'Root dir: '. ROOT_DIR . '<br>';
-    //require_once '../app/controllers/Images_test.php';
-
-    echo file_exists('C:\xampp\htdocs\30_PHP_gallery\public\img\2007 - Буря.jpg');
